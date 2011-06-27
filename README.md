e746f6d9fe03e20f16cc4a3e9581ff38cf175eef
Marius Butuc <marius.butuc@ca.ibm.com>
2011-06-27 15:57:02 -0400

    Added ajax magic

--- README.md
+++ README.md
@@ -3,12 +3,17 @@
 Creating a 100% ajax CRUD using Rails 3.1 and UJS,
 inspired by [stJhimy](http://stjhimy.com/posts/7-creating-a-100-ajax-crud-using-rails-3-and-unobtrusive-javascript).
 
-Steps in augmenting the default (nifty) scaffold:
+### Steps in augmenting the default (nifty) scaffold:
 
 1. Create a unique form in `/posts` that will do everything with ajax:
 `index`, `create`, `edit` and `destroy`. Removed `new` and `show`.
+2. Add ajax magic:
+   - remote requests, both in controller and links
+   - placeholder divs to be handled with UJS
+   - remove the redirects from the controller
+   - the actions return JS, to change the DOM
 
-PoC covering:
+### PoC covering:
 
 - Rails 3.1
 - UJS with jQuery
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,29 +12,29 @@ class PostsController < ApplicationController
   def create
     @post = Post.new(params[:post])
     if @post.save
-      redirect_to posts_url, notice: 'Successfully created post.'
-    else
-      render action: :index
+      flash[:notice] = 'Successfully created post.'
+      # Reload all posts
+      @posts = Post.all
     end
   end
 
   def edit
     @post = Post.find(params[:id])
-    render action: :index
   end
 
   def update
     @post = Post.find(params[:id])
     if @post.update_attributes(params[:post])
-      redirect_to post_url, notice: 'Successfully updated post.'
-    else
-      render action: :index
+      flash[:notice] = 'Successfully updated post.'
+      # Again, reload all posts
+      @posts = Post.all
     end
   end
 
   def destroy
     @post = Post.find(params[:id])
     @post.destroy
-    redirect_to posts_url, notice: 'Successfully destroyed post.'
+    flash[:notice] = 'Successfully destroyed post.'
+    @posts = Post.all
   end
 end
